f78712675315e5b171f31aeea5f3947a771e5910
Adriano Souza <adrianosouza11@gmail.com>
2018-11-17 01:43:56 -0200

    Solution curing delimitadas

---
+++ src/entities/Rectangle.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Rectangle implements Shape {
+    private Double width;
+    private Double height;
+
+    public Rectangle(){
+
+    }
+
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    @Override
+    public double area(){
+        return width * height;
+    }
+}
